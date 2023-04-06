8767ee9eb969707a5f7cfb536ad0c731b9a396d7
Matt-Hoang <61717506+Matt-Hoang@users.noreply.github.com>
2023-04-05 23:57:55 -0700

    pull checklist from db

--- javascript/itineraries.js
+++ javascript/itineraries.js
@@ -30,7 +30,7 @@ var myNodelist = document.getElementsByTagName("LI");
 for (var i = 0; i < myNodelist.length; i++) {
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
-  span.className = "close";  
+  span.className = "close";
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
 }
@@ -38,7 +38,7 @@ for (var i = 0; i < myNodelist.length; i++) {
 // Click on a close button to hide the current list item
 var close = document.getElementsByClassName("close");
 for (var i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
+  close[i].onclick = function () {
     var div = this.parentElement;
     div.style.display = "none";
   }
@@ -47,7 +47,7 @@ for (var i = 0; i < close.length; i++) {
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelectorAll('ul');
 for (let i = 0; i < list.length; i++) {
-  list[i].addEventListener('click', function(ev) {
+  list[i].addEventListener('click', function (ev) {
     if (ev.target.tagName === 'LI') {
       ev.target.classList.toggle('checked');
     }
@@ -55,6 +55,7 @@ for (let i = 0; i < list.length; i++) {
 }
 
 // Create a new list item when clicking on the "Add" button for pre-trip check list
+window.newElementPre = newElementPre;
 function newElementPre() {
   var inputValue = document.getElementsByClassName("pretrip-checklist-input")
   for (var i = 0; i < inputValue.length; i++) {
@@ -78,15 +79,16 @@ function newElementPre() {
     li.appendChild(span);
 
     for (i = 0; i < close.length; i++) {
-      close[i].onclick = function() {
+      close[i].onclick = function () {
         var div = this.parentElement;
         div.style.display = "none";
       }
     }
-  } 
+  }
 }
 
 // Create a new list item when clicking on the "Add" button for post-trip check list
+window.newElementPost = newElementPost;
 function newElementPost() {
   var inputValue = document.getElementsByClassName("posttrip-checklist-input")
   for (var i = 0; i < inputValue.length; i++) {
@@ -110,10 +112,131 @@ function newElementPost() {
     li.appendChild(span);
 
     for (i = 0; i < close.length; i++) {
-      close[i].onclick = function() {
+      close[i].onclick = function () {
         var div = this.parentElement;
         div.style.display = "none";
       }
     }
-  } 
+  }
 }
+
+
+
+// write to firebase
+import { firebaseConfig } from "./firebaseKey.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
+import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-database.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-auth.js";
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const auth = getAuth();
+
+// write item to list 
+function writeUserData(user) {
+  var itemName1 = "Item 1";
+  var itemName2 = "Item 2";
+
+  var mark = 0;
+
+  set(ref(database, 'users/' + user.uid + "/checklists/pretrip/" + itemName1), {
+    strikethrough: mark
+  });
+
+  set(ref(database, 'users/' + user.uid + "/checklists/pretrip/" + itemName2), {
+    strikethrough: mark
+  });
+}
+
+
+
+
+
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in
+
+    // read and display pretrip item from database 
+    const pretripChecklist = ref(database, 'users/' + user.uid + "/checklists/pretrip/");
+    onValue(pretripChecklist, (snapshot) => {
+      const checklistItems = snapshot.val();
+
+      // parse pretrip checklist 
+      for (var itemName in checklistItems) {
+        // add item to checklist
+        var li = document.createElement("li");
+        var t = document.createTextNode(itemName);
+        li.appendChild(t);
+
+        var ul = document.getElementsByClassName("pretrip-checklist")
+        for (var j = 0; j < ul.length; j++) {
+          ul[j].appendChild(li);
+        }
+
+        // create and add "x" to item
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        li.appendChild(span);
+
+        for (i = 0; i < close.length; i++) {
+          close[i].onclick = function () {
+            var div = this.parentElement;
+            div.style.display = "none";
+          }
+        }
+
+
+        // mark item as completed (strikethrough and checkmark)
+        if (Object.values(checklistItems[itemName]) == 1) {
+          li.className = "checked";
+        }
+      }
+    });
+
+    // read and display posttrip item from database
+    const posttripChecklist = ref(database, 'users/' + user.uid + "/checklists/posttrip/");
+    onValue(posttripChecklist, (snapshot) => {
+      const checklistItems = snapshot.val();
+
+      // parse posttrip checklist 
+      for (var itemName in checklistItems) {
+        // add item to checklist
+        var li = document.createElement("li");
+        var t = document.createTextNode(itemName);
+        li.appendChild(t);
+
+        var ul = document.getElementsByClassName("posttrip-checklist")
+        for (var j = 0; j < ul.length; j++) {
+          ul[j].appendChild(li);
+        }
+
+        // create and add "x" to item
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        li.appendChild(span);
+
+        for (i = 0; i < close.length; i++) {
+          close[i].onclick = function () {
+            var div = this.parentElement;
+            div.style.display = "none";
+          }
+        }
+
+        // mark item as completed (strikethrough and checkmark)
+        if (Object.values(checklistItems[itemName]) == 1) {
+          li.className = "checked";
+        }
+      }
+    });
+
+  } else {
+    // User is signed out
+    console.log("not logged in");
+  }
+});
