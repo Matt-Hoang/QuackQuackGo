abfb9ce73a0486bcab1f151692f402f0b1fd43b9
Matt-Hoang <61717506+Matt-Hoang@users.noreply.github.com>
2023-04-06 14:57:10 -0700

    updated weather

--- javascript/weather.js
+++ javascript/weather.js
@@ -5,7 +5,7 @@ let weather = {
         fetch(
             "https://api.openweathermap.org/data/2.5/weather?q="
             + city 
-            + "&units=metric&appid=" 
+            + "&units=imperial&appid=" 
             + apiKey
         ).then((response) => response.json())
         .then((data) =>this.displayWeather(data));
@@ -19,9 +19,9 @@ let weather = {
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon +".png";
         document.querySelector(".description").innerText = description;
-        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".temp").innerText = temp + "°F";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity +"%";
-        document.querySelector(".wind").innerText = "Wind speed: " + speed +" km/h";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed +" mph";
         document.querySelector(".weather").classList.remove("loading")
     },
     search:function() {
